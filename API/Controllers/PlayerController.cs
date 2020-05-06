5ae3d5c8efda8909148a7c9bf95b92755846cd2d
Alec Perkins <alec.1824@gmail.com>
2020-05-05 20:32:53 -0400

    added full CRUD for player table

---
+++ API/Controllers/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using Data;
+using Microsoft.AspNet.Identity;
+using Models;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class PlayerController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            PlayerService playerService = CreatePlayerService();
+            var players = playerService.GetPlayers();
+            return Ok(players);
+        }
+
+        public IHttpActionResult Post(PlayerCreate player)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePlayerService();
+
+            if (!service.CreatePlayer(player))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        private PlayerService CreatePlayerService()
+        {
+            var playerId = Guid.Parse(User.Identity.GetUserId());
+            var playerService = new PlayerService(playerId);
+            return playerService;
+        }
+
+        public IHttpActionResult Get(string name)
+        {
+            PlayerService playerService = CreatePlayerService();
+            var player = playerService.GetPlayerByName(name);
+            return Ok();
+        }
+
+        public IHttpActionResult Put(PlayerEdit player)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var service = CreatePlayerService();
+
+            if (!service.UpdatePlayer(player))
+                return InternalServerError();
+
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(string name)
+        {
+            var service = CreatePlayerService();
+
+            if (!service.DeletePlayer(name))
+                return InternalServerError();
+
+            return Ok();
+        }
+    }
+}
---
+++ Data/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Player
+    {
+        [Key]
+        public Guid playerId { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "You have to enter at least 1 character.")]
+        [MaxLength(40, ErrorMessage = "You entered too many characters.")]
+        public string name { get; set; }
+
+        [Required]
+        public int playerNumber { get; set; }
+
+        [Required]
+        [MinLength(4, ErrorMessage = "You have to enter at least 4 characters.")]
+        [MaxLength(30, ErrorMessage = "You entered too many characters.")]
+        public string playerPosition { get; set; }
+        
+        [Required]
+        public int yearsPro { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "You have to enter at least 5 characters.")]
+        [MaxLength(30, ErrorMessage = "You entered too many characters.")]
+        public string team { get; set; }
+
+        [Required]
+        public int numberOfVotes { get; set; }
+
+        [Required]
+        public DateTimeOffset createdUtc { get; set; }
+
+        public DateTimeOffset? modifiedUtc { get; set; }
+    }
+}
