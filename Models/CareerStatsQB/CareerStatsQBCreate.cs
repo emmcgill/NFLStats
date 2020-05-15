139d75380426390fe28121c3c9f53537ba1d6c29
Todd Newland <tnewland08@gmail.com>
2020-05-14 21:13:32 -0400

    fixed QB Models and Services

---
+++ Models/CareerStatsQB/CareerStatsQBCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.CareerStatsQBModels
+{
+    public class CareerStatsQBCreate
+    {
+        public int PlayerId { get; set; }
+
+        [Required]
+        public int PassingYards { get; set; }
+
+        [Required]
+        public int RushingYards { get; set; }
+
+        [Required]
+        public int Completions { get; set; }
+
+        [Required]
+        public int Attempts { get; set; }
+
+        [Required]
+        public int PassingTouchdowns { get; set; }
+
+        [Required]
+        public int RushingTouchdowns { get; set; }
+
+        [Required]
+        public int Interceptions { get; set; }
+    }
+}
