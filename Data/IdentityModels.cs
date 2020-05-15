72ad213f0cbdac340136a2a917ed46c6f59f2ef5
Todd Newland <tnewland08@gmail.com>
2020-05-14 21:20:48 -0400

    fixed QB Models and Services

--- Data/IdentityModels.cs
+++ Data/IdentityModels.cs
@@ -36,6 +36,8 @@ namespace Data
         public DbSet<Player> Players { get; set; }
         public DbSet<SeasonStat> SeasonStats { get; set; }
 
+        public DbSet<CareerStatsQB> careerStatsQBs { get; set; }
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder
