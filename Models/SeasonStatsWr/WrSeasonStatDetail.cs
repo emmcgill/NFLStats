9edbea5b2a3d702fa6247f942bcc0dbbc239ec9e
emmcgill <mcgill.erik@gmail.com>
2020-05-16 13:30:03 -0400

    WrSeasonStats Added

---
+++ Models/SeasonStatsWr/WrSeasonStatDetail.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.SeasonStatsWr
+{
+    public class WrSeasonStatDetail
+    {
+        public int WrSeasonId { get; set; }
+
+        public int PlayerId { get; set; }
+
+        public int Year { get; set; }
+
+        public int Receptions { get; set; }
+
+        public int Targets { get; set; }
+
+        public int Drops { get; set; }
+
+        [Display(Name = "Receiving Yards")]
+        public int ReceivingYards { get; set; }
+
+        [Display(Name = "Yards After Catch")]
+        public int YardsAfterCatch { get; set; }
+
+        public int Touchdowns { get; set; }
+    }
+}
